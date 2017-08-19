2519cee3a6201b8cc665f81146c51e4f9dfc1d10
James McElwain <jmcelwain@gmail.com>
2017-08-19 00:16:16 -0500

    add comment about stupid 'no handler' errors

--- src/gen_art/Color/P122.clj
+++ src/gen_art/Color/P122.clj
@@ -2,26 +2,29 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
-(defn path [x] (clojure.java.io/as-relative-path x))
-
-(def img (q/load-image "a.jpg"))
-
 (defn setup []
-  (q/no-cursor)
-  (q/no-stroke)
-  (q/color-mode :hsb 360 100 100)
-  (q/frame-rate 30))
+  ;; (def img (q/load-image "a.jpg"))
+  ;; (q/no-cursor)
+  ;; (q/no-stroke)
+  ;; (q/color-mode :hsb 360 100 100)
+  ;; (q/frame-rate 30)
+  )
 
-(defn update-state [])
+(defn update-state [state] state)
 
-(defn draw-state []
-  (let [tile-count (/ (q/width) (max (q/mouse-x) 5))
-        rect-size (/ (q/width) tile-count)]))
+(defn draw-state [state]
+  (q/background 0)
+  (let [img (q/load-image "a.jpg")]
+    (println (.toString img)))
+  ;; (let [tile-count (/ (q/width) (max (q/mouse-x) 5))
+  ;;       rect-size (/ (q/width) tile-count)])
+  )
 
 (q/defsketch gen-art
   :title "Color palettes from images"
   :size [600 600]
   :setup setup
-  :draw: draw-state
-  :features [:keep-on-top]
+  :draw draw-state
+  ;; We need no-bind-output not to break things, this also allows us to write to std out in cider
+  :features [:no-bind-output]
   :middleware [m/fun-mode])
