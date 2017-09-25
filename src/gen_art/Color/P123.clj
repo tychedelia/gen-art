72ed2b165aa56093430d81dcaff7b5f64b10e527
James McElwain <jmcelwain@gmail.com>
2017-09-24 19:39:14 -0500

    initial pass on 123

--- src/gen_art/Color/P123.clj
+++ src/gen_art/Color/P123.clj
@@ -8,19 +8,30 @@
 (defn setup []
   (q/color-mode :hsb 360 100 100 100)
   (q/no-stroke)
-  (let [hue-values        (map (partial q/random 0 360) (range tile-count-x))
-        saturation-values (map (partial q/random 0 100) (range tile-count-x))
-        brightness-values (map (partial q/random 0 100) (range tile-count-x))]
+  (let [hue-values        (mapv (fn [& _] (rand-int 360)) (range tile-count-x))
+        saturation-values (mapv (fn [& _] (rand-int 100)) (range tile-count-x))
+        brightness-values (mapv (fn [& _] (rand-int 100)) (range tile-count-x))]
+    (println hue-values)
     {:h hue-values
      :s saturation-values
      :b brightness-values}))
 
-(defn update-state [state])
+(defn update-state [state] state)
 
 (defn draw-state [state]
   (q/background 0 0 100)
-  (println (:h state))
-  )
+  (let [current-tile-x (q/map-range (q/mouse-x) 0 (q/width)  1 tile-count-x)
+        current-tile-y (q/map-range (q/mouse-y) 0 (q/height) 1 tile-count-y)
+        tile-width  (/ (q/width)  current-tile-x)
+        tile-height (/ (q/height) current-tile-y)
+       !i           (atom 0)]
+  (doseq [x (range tile-count-x)
+          y (range tile-count-y)
+          :let [pos-x (* tile-width  x)
+                pos-y (* tile-height y)]]
+    (q/fill (nth (:h state) @!i) (nth (:s state) @!i) (nth (:b state) @!i))
+    (q/rect pos-x pos-y tile-width tile-height)
+    (swap! !i inc))))
 
 (q/defsketch gen-art
   :title "Color palettes from rules"
