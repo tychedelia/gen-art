2fe1628e399cdddb5e2ee34835e8009509faa10e
JCM <jmcelwain@gmail.com>
2019-10-20 21:15:33 -0700

    Remove reflection, unecessary dynamic calls

--- src/gen_art/Sketch/lissa.clj
+++ src/gen_art/Sketch/lissa.clj
@@ -11,34 +11,39 @@
             :line-weight 1
             :line-color 0
             :line-alpha 50
-            :connection-radius 80
+            :phi 15
+            :connection-radius 70
             :connection-ramp 6})
 
-;; VARS
-(def !phi (atom 15))
 
 ;; LIFECYCLE
-(defn calc-points [{:keys [point-count freq-x freq-y mod-freq-x mod-freq-y points]}]
+(defn update-points [{:keys [point-count freq-x freq-y mod-freq-x mod-freq-y phi points] :as state}]
   (doseq [i (range point-count)
           :let [angle         (q/map-range i 0 point-count 0 q/TWO-PI)
-                x             (* (q/sin (+ (q/radians @!phi) (* angle freq-x))) (q/cos (* angle mod-freq-x)))
+                x             (* (q/sin (+ (q/radians phi) (* angle freq-x))) (q/cos (* angle mod-freq-x)))
                 y             (* (q/sin (* angle freq-y)) (q/cos (* angle mod-freq-y)))
+                ^"[[F" points points
                 ^floats point (aget points i)]]
     (aset point 0 (float (* x (- (/ (q/width) 2 ) 30))))
-    (aset point 1 (float (* y (- (/ (q/height) 2) 30))))))
-
-(defn update-vars []
-  (swap! !phi inc))
+    (aset point 1 (float (* y (- (/ (q/height) 2) 30)))))
+  state)
 
 (defn setup []
+  (q/frame-rate 60)
   (let [state (assoc state :points (make-array Float/TYPE (:point-count state) 2))]
-    (calc-points state)
-    state))
+    (update-points state)))
+
+(defn update-vars [state]
+  (assoc state
+         :phi (inc (:phi state))
+         :mod-freq-x (+ 0.001 (:mod-freq-x state))
+         :mod-freq-y (+ 0.001 (:mod-freq-y state))
+         ))
 
 (defn update [state]
-  (update-vars)
-  (calc-points state)
-  state)
+  (-> state
+      (update-vars)
+      (update-points)))
 
 (defn lissa [{:keys [line-weight point-count connection-radius line-color line-alpha points]}]
   (q/color-mode :rgb)
@@ -54,13 +59,17 @@
   (doseq [i1 (range point-count)
           i2 (range i1)
           :let [^"[[F" points points
-                [x1 y1] (aget points i1)
-                [x2 y2] (aget points i2)
+                ^floats p1 (aget points i1)
+                ^floats p2 (aget points i2)
+                x1 (aget p1 0)
+                y1 (aget p1 1)
+                x2 (aget p2 0)
+                y2 (aget p2 1)
                 d       (q/dist x1 y1 x2 y2)
                 a       (q/pow (/ 1 (/ d (+ 1 connection-radius))) 6)]]
 
     (if (< d connection-radius)
-      (do (q/stroke line-color (* a line-alpha))
+      (do (q/stroke line-color (min Float/MAX_VALUE (* a line-alpha)))
           (q/line x1 y1 x2 y2))))
   (q/pop-matrix))
 
