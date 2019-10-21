acfc8fbb83ac6f71119a755902226224f27c6ddd
JCM <jmcelwain@gmail.com>
2019-10-20 20:40:27 -0700

    Optimize

--- src/gen_art/Sketch/lissa.clj
+++ src/gen_art/Sketch/lissa.clj
@@ -3,44 +3,44 @@
             [quil.middleware :as m]))
 
 ;; CONSTANTS
-(def settings { :point-count 200
-               :freq-x 4
-               :freq-y 7
-               :mod-freq-x 3
-               :mod-freq-y 2
-               :line-weight 1
-               :line-color 0
-               :line-alpha 50
-               :connection-radius 80
-               :connection-ramp  6 })
+(def state {:point-count 200
+            :freq-x 4
+            :freq-y 7
+            :mod-freq-x 3
+            :mod-freq-y 2
+            :line-weight 1
+            :line-color 0
+            :line-alpha 50
+            :connection-radius 80
+            :connection-ramp 6})
 
 ;; VARS
-(def !phi         (atom 15))
-
-;; UTIL
-(defn point [x y] [x y])
+(def !phi (atom 15))
 
 ;; LIFECYCLE
-(defn calc-points [{:keys [point-count freq-x freq-y mod-freq-x mod-freq-y]}]
-  (for [i (range point-count)
-        :let [angle (q/map-range i 0 point-count 0 q/TWO-PI)
-              x     (* (q/sin (+ (q/radians @!phi) (* angle freq-x))) (q/cos (* angle mod-freq-x)))
-              y     (* (q/sin (* angle freq-y)) (q/cos (* angle mod-freq-y)))]]
-    (point
-     (* x (- (/ (q/width) 2 ) 30))
-     (* y (- (/ (q/height) 2) 30)))))
+(defn calc-points [{:keys [point-count freq-x freq-y mod-freq-x mod-freq-y points]}]
+  (doseq [i (range point-count)
+          :let [angle         (q/map-range i 0 point-count 0 q/TWO-PI)
+                x             (* (q/sin (+ (q/radians @!phi) (* angle freq-x))) (q/cos (* angle mod-freq-x)))
+                y             (* (q/sin (* angle freq-y)) (q/cos (* angle mod-freq-y)))
+                ^floats point (aget points i)]]
+    (aset point 0 (float (* x (- (/ (q/width) 2 ) 30))))
+    (aset point 1 (float (* y (- (/ (q/height) 2) 30))))))
 
 (defn update-vars []
-  (swap! !phi (fn [x] (q/random 0 360))))
+  (swap! !phi inc))
 
 (defn setup []
-  (calc-points settings))
+  (let [state (assoc state :points (make-array Float/TYPE (:point-count state) 2))]
+    (calc-points state)
+    state))
 
-(defn update [points]
+(defn update [state]
   (update-vars)
-  (calc-points settings))
+  (calc-points state)
+  state)
 
-(defn lissa [{:keys [line-weight point-count connection-radius line-color line-alpha]} points]
+(defn lissa [{:keys [line-weight point-count connection-radius line-color line-alpha points]}]
   (q/color-mode :rgb)
   (q/background 255)
   (q/stroke-weight line-weight)
@@ -53,18 +53,19 @@
   ;; LOOP
   (doseq [i1 (range point-count)
           i2 (range i1)
-          :let [[x1 y1] (nth points i1)
-                [x2 y2] (nth points i2)
+          :let [^"[[F" points points
+                [x1 y1] (aget points i1)
+                [x2 y2] (aget points i2)
                 d       (q/dist x1 y1 x2 y2)
                 a       (q/pow (/ 1 (/ d (+ 1 connection-radius))) 6)]]
 
     (if (< d connection-radius)
       (do (q/stroke line-color (* a line-alpha))
-          (q/line x1 y1 x2 y2)
-          (q/pop-matrix)))))
+          (q/line x1 y1 x2 y2))))
+  (q/pop-matrix))
 
-(defn draw [points]
-  (lissa settings points))
+(defn draw [state]
+  (lissa state))
 
 (q/defsketch gen-art
   :title "Lissajous"
